e3da3d093ab5466a7b4fd713d950d92aa1929bda
LeeJoEun-01 <joeun1005@naver.com>
2023-11-18 14:29:15 +0900

    [set] 프로젝트 세팅

---
+++ Airbnb-iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Airbnb-iOS
+//
+//  Created by 이조은 on 2023/11/18.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
+
