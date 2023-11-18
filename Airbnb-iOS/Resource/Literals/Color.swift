53e5efc0d54997a7ecd2f5a87e0a6f8b7e74459c
Zoe <mam07065@naver.com>
2023-11-18 16:13:51 +0900

    [style] 디자인시스템 구축

--- Airbnb-iOS/Resource/Literals/Color.swift
+++ Airbnb-iOS/Resource/Literals/Color.swift
@@ -5,4 +5,34 @@
 //  Created by 지희의 MAC on 2023/11/18.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+  @nonobjc class var grayGray1: UIColor {
+    return UIColor(white: 177.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var grayGrayText: UIColor {
+    return UIColor(white: 111.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var grayBackGray1: UIColor {
+    return UIColor(white: 247.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var black1: UIColor {
+    return UIColor(white: 34.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var main1: UIColor {
+    return UIColor(red: 221.0 / 255.0, green: 15.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var grayGrayIcon: UIColor {
+    return UIColor(white: 112.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var whiteWhite40: UIColor {
+    return UIColor(white: 1.0, alpha: 0.4)
+  }
+  @nonobjc class var grayGrayLine: UIColor {
+    return UIColor(white: 232.0 / 255.0, alpha: 1.0)
+  }
+  @nonobjc class var whiteWhite: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+}
--- Airbnb-iOS/Resource/Literals/Font.swift
+++ Airbnb-iOS/Resource/Literals/Font.swift
@@ -5,4 +5,40 @@
 //  Created by 지희의 MAC on 2023/11/18.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+  class var iosTitleSemi32: UIFont {
+    return UIFont(name: "Poppins-SemiBold", size: 32.0)!
+  }
+  class var iosTitleBold25: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Bold", size: 25.0)!
+  }
+  class var iosBodyBold18Num: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Bold", size: 18.0)!
+  }
+  class var iosSubMedium16: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 16.0)!
+  }
+  class var iosCapMedium16Underline: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 16.0)!
+  }
+  class var iosSubMedium15: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 15.0)!
+  }
+  class var iosBodyBold14: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Bold", size: 14.0)!
+  }
+  class var iosBodyMedium14: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 14.0)!
+  }
+  class var iosCapMedium125Underline: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 12.5)!
+  }
+  class var iosCapMedium125: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 12.5)!
+  }
+  class var iosSubMedium10: UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Medium", size: 10.0)!
+  }
+}
