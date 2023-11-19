877afef3fe5c880d28819d013f7e42f792703ab9
Zoe <mam07065@naver.com>
2023-11-19 23:02:52 +0900

    [add] 에셋 추가

--- Airbnb-iOS/Resource/Literals/ImageLiteral.swift
+++ Airbnb-iOS/Resource/Literals/ImageLiteral.swift
@@ -5,4 +5,33 @@
 //  Created by 지희의 MAC on 2023/11/18.
 //
 
-import Foundation
+import UIKit
+
+class ImageLiteral {
+    static let icExploreBtmMessage = UIImage(named: "ic_explore_btm_message")!
+    static let icExploreBtmProfile = UIImage(named: "ic_explore_btm_profile")!
+    static let icExploreBtmSearch = UIImage(named: "ic_explore_btm_search")!
+    static let icExploreBtmTrip = UIImage(named: "ic_explore_btm_trip")!
+    static let icExploreBtmWishlist = UIImage(named: "ic_explore_btm_wishlist")!
+    
+    static let icExploreFiltering = UIImage(named: "ic_explore_filtering")!
+    static let icExploreGoodview = UIImage(named: "ic_explore_goodview")!
+    static let icExploreMap = UIImage(named: "ic_explore_map")!
+    static let icExploreRoom = UIImage(named: "ic_explore_room")!
+    static let icExploreSeashore = UIImage(named: "ic_explore_seashore")!
+    static let icExploreTreehouse = UIImage(named: "ic_explore_treehouse")!
+    
+    static let icHeart = UIImage(named: "ic_heart")!
+    static let icHeartFill = UIImage(named: "ic_heart_fill")!
+    
+    static let icExploreSearch = UIImage(named: "ic_explore_search")!
+    
+    static let imgExplore = UIImage(named: "img_explore")!
+    static let europe = UIImage(named: "europe")!
+    static let global = UIImage(named: "global")!
+    static let japan = UIImage(named: "japan")!
+    
+    static let btnMinus = UIImage(named: "btn_minus")!
+    static let btnPlus = UIImage(named: "btn_plus")!
+    
+}
