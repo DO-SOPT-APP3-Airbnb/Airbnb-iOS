0fb1eb00200894c1891bb973bc3500a5af646bcb
Zoe <Zoe.urssu@gmail.com>
2023-11-21 04:48:28 +0900

    [Add/#3] UIButton Extension 추가

---
+++ Airbnb-iOS/Source/Extension/UIButton+.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton+.swift
+//  Airbnb-iOS
+//
+//  Created by 지희의 MAC on 2023/11/21.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
