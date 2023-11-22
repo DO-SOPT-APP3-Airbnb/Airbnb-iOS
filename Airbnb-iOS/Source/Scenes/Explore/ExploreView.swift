35eada29f7087281a0c0b6f33ee601293474fbe5
LeeJoEun-01 <joeun1005@naver.com>
2023-11-22 13:51:49 +0900

    [Feat/#2] WhereView로 넘어가느 버튼 UI

--- Airbnb-iOS/Source/Scenes/Explore/ExploreView.swift
+++ Airbnb-iOS/Source/Scenes/Explore/ExploreView.swift
@@ -10,6 +10,56 @@ import UIKit
 import SnapKit
 
 final class ExploreView: UIView {
+    // MARK: - Properties
+    ///WhereView로 넘어가는 버튼
+    private let gotoTravelButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .whiteWhite
+        button.layer.cornerRadius = 29.5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.grayGrayLine.cgColor
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.05).cgColor
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowOffset = CGSize.zero
+        button.layer.shadowRadius = 8.8
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+
+    private let gotoTravelView = UIView()
+
+    private let searchImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiteral.icExploreSearch
+
+        return imageView
+    }()
+
+    private let whereLabel: UILabel = {
+        let label = UILabel()
+        label.text = "어디로 여행가세요?"
+        label.textColor = .black1
+        label.font = .iosCapMedium125
+
+        return label
+    }()
+
+    private let whenLabel: UILabel = {
+        let label = UILabel()
+        label.text = "어디든지•언제든 일주일•게스트 추가"
+        label.textColor = .grayGrayText
+        label.font = .iosCapMedium125
+
+        return label
+    }()
+
+    private let filterImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiteral.icExploreFiltering
+
+        return imageView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,12 +77,37 @@ final class ExploreView: UIView {
     }
 
     func setHierarchy() {
-        // self.addSubview(scrollView)
+        self.addSubview(gotoTravelButton)
+        gotoTravelButton.addSubview(gotoTravelView)
+        gotoTravelView.addSubviews(searchImage, whereLabel, whenLabel, filterImage)
     }
 
     func setConstraints() {
-//        view.snp.makeConstraints {
-//            $0.edges.equalToSuperview()
-//        }
+        gotoTravelButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(62)
+            $0.height.equalTo(56)
+            $0.leading.trailing.equalToSuperview().inset(13)
+        }
+        gotoTravelView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        searchImage.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(14)
+            $0.size.equalTo(28)
+            $0.leading.equalToSuperview().inset(19)
+        }
+        whereLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(12)
+            $0.leading.equalTo(searchImage.snp.trailing).offset(14)
+        }
+        whenLabel.snp.makeConstraints {
+            $0.top.equalTo(whereLabel.snp.bottom).offset(1)
+            $0.leading.equalTo(searchImage.snp.trailing).offset(14)
+        }
+        filterImage.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.size.equalTo(36)
+            $0.trailing.equalToSuperview().inset(15)
+        }
     }
 }
