67eba6586912a591db9a971708166d9dab5f910a
LeeJoEun-01 <joeun1005@naver.com>
2023-11-25 15:17:36 +0900

    [Chore/#10] 주석 제거 및 indent 수정

--- Airbnb-iOS/Source/Scenes/Explore/ExploreViewController.swift
+++ Airbnb-iOS/Source/Scenes/Explore/ExploreViewController.swift
@@ -10,33 +10,33 @@ import UIKit
 import SnapKit
 
 final class ExploreViewController: UIViewController {
-
+    
     let exploreView = ExploreView()
-
+    
     let themaLables: [String] = ["통나무집", "방", "최고의 전망", "해변 근처"]
     let tnemaIcons: [UIImage] = [ImageLiteral.icExploreTreehouse, ImageLiteral.icExploreRoom, ImageLiteral.icExploreGoodview, ImageLiteral.icExploreSeashore]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteWhite
-
+        
         exploreView.themaCollectionView.delegate = self
         exploreView.themaCollectionView.dataSource = self
         exploreView.themaCardCollectionView.delegate = self
         exploreView.themaCardCollectionView.dataSource = self
-
+        
         setUI()
     }
-
+    
     func setUI() {
         setHierarchy()
         setConstraints()
     }
-
+    
     func setHierarchy() {
         self.view.addSubview(exploreView)
     }
-
+    
     func setConstraints() {
         exploreView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -53,7 +53,7 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
             return 5
         }
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == exploreView.themaCollectionView {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThemaCollectionViewCell.identifier, for: indexPath) as! ThemaCollectionViewCell
@@ -68,7 +68,7 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
             return cell
         }
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         if collectionView == exploreView.themaCollectionView {
             return 31
@@ -76,7 +76,7 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
             return 8
         }
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == exploreView.themaCollectionView {
             return CGSize(width: 54, height: 72)
@@ -84,7 +84,7 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
             return CGSize(width: 348, height: 464)
         }
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         if collectionView == exploreView.themaCardCollectionView {
             let deviceWidth = UIScreen.main.bounds.width
@@ -97,16 +97,16 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
         }
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let index = indexPath.row
         if collectionView == exploreView.themaCollectionView {
             print("클릭: \(themaLables[index])")
         }
     }
-
+    
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         let page = Int(targetContentOffset.pointee.x / 348)
         exploreView.pageControl.currentPage = page
-      }
+    }
 }
