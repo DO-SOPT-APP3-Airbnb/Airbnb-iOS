1b0b5606f531ad09190bbdf7b14e83db8826e7fb
Zoe <Zoe.urssu@gmail.com>
2023-11-28 16:10:06 +0900

    [Network/#16] 네트워크 세팅

---
+++ Airbnb-iOS/Source/Services/NetworkError.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkError.swift
+//  Airbnb-iOS
+//
+//  Created by 지희의 MAC on 2023/11/28.
+//
+
+import Foundation
+
+enum NetworkError: Int, Error, CustomStringConvertible {
+    var description: String { self.errorDescription }
+    case requstEncodingError
+    case responseDecodingError
+    case responseError
+    case unknownError
+    case internalServerError = 500
+    case notFoundError = 404
+    
+    var errorDescription: String {
+        switch self {
+        case .requstEncodingError: return "REQUEST_ENCODING_ERROR"
+        case .responseError: return "RESPONSE_ERROR"
+        case .responseDecodingError: return "RESPONSE_DECODING_ERROR"
+        case .unknownError: return "UNKNOWN_ERROR"
+        case .internalServerError: return "500:INTERNAL_SERVER_ERROR"
+        case .notFoundError: return "404:NOT_FOUND_ERROR"
+        }
+    }
+}
