850d372a5b1a877eb97e4ad6ed0fe7331fee513f
LeeJoEun-01 <joeun1005@naver.com>
2023-11-30 00:21:48 +0900

    [Feat/#15] ExploreView - API 연결

--- Airbnb-iOS/Source/Scenes/Explore/ExploreViewController.swift
+++ Airbnb-iOS/Source/Scenes/Explore/ExploreViewController.swift
@@ -10,25 +10,29 @@ import UIKit
 import SnapKit
 
 final class ExploreViewController: UIViewController {
-    
+
     let exploreView = ExploreView()
-    
+
     let themaLables: [String] = ["통나무집", "방", "최고의 전망", "해변 근처"]
     let tnemaIcons: [UIImage] = [ImageLiteral.icExploreTreehouse, ImageLiteral.icExploreRoom, ImageLiteral.icExploreGoodview, ImageLiteral.icExploreSeashore]
-    
+
+    var themaCardInfo: ThemaCardDTO?
+    var themaCardImage: ThemaCardImageDTO?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteWhite
-        
+
         exploreView.themaCollectionView.delegate = self
         exploreView.themaCollectionView.dataSource = self
         exploreView.themaCardCollectionView.delegate = self
         exploreView.themaCardCollectionView.dataSource = self
-        
+
         setUI()
         addTarget()
 
         self.navigationController?.isNavigationBarHidden = true
+        getThemaCard(id: 1)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -39,11 +43,11 @@ final class ExploreViewController: UIViewController {
         setHierarchy()
         setConstraints()
     }
-    
+
     func setHierarchy() {
         self.view.addSubview(exploreView)
     }
-    
+
     func setConstraints() {
         exploreView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -68,10 +72,11 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
         if collectionView == exploreView.themaCollectionView {
             return themaLables.count
         } else {
-            return 5
+            self.exploreView.pageControl.numberOfPages = themaCardInfo?.data.count ?? 0
+            return themaCardInfo?.data.count ?? 0
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == exploreView.themaCollectionView {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThemaCollectionViewCell.identifier, for: indexPath) as! ThemaCollectionViewCell
@@ -83,10 +88,16 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
         } else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThemaCardCollectionViewCell.identifier, for: indexPath) as! ThemaCardCollectionViewCell
             // data binding
+            cell.setThemaCard(title: themaCardInfo?.data[indexPath.row].description ?? "",
+                              distance: String(themaCardInfo?.data[indexPath.row].distance ?? 0),
+                              date: themaCardInfo?.data[indexPath.row].travelDate ?? "",
+                              cost: String(themaCardInfo?.data[indexPath.row].price ?? 0),
+                              star: String(themaCardInfo?.data[indexPath.row].scope ?? 0))
+            cell.themaCard.setImage(urlString: themaCardImage?.data[indexPath.row].imageURL ?? "", defaultImage: ImageLiteral.imgExplore)
             return cell
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         if collectionView == exploreView.themaCollectionView {
             return 31
@@ -94,7 +105,7 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
             return 8
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == exploreView.themaCollectionView {
             return CGSize(width: 54, height: 72)
@@ -102,7 +113,7 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
             return CGSize(width: 348, height: 464)
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         if collectionView == exploreView.themaCardCollectionView {
             let deviceWidth = UIScreen.main.bounds.width
@@ -115,16 +126,59 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDeleg
         }
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let index = indexPath.row
         if collectionView == exploreView.themaCollectionView {
             print("클릭: \(themaLables[index])")
+            getThemaCard(id: index+1)
         }
     }
-    
+
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         let page = Int(targetContentOffset.pointee.x / 348)
+        print("===page: \(page)")
+        print("===targetContentOffset: \(targetContentOffset.pointee.x)")
         exploreView.pageControl.currentPage = page
+    }
+}
+
+extension ExploreViewController {
+    func getThemaCard(id: Int) {
+        themaCardService().getThemaCard(id: id) { result in
+            switch result {
+            case .success(let themaCardResponse):
+                self.themaCardInfo = themaCardResponse
+                if let themaCardInfo = self.themaCardInfo {
+                    self.getThemaCardImage(id: id)
+                } else {
+                    print("RESPONSE ERROR")
+                }
+            case .failure(let error):
+                print("API Error: \(error)")
+            }
+        }
+    }
+
+    func getThemaCardImage(id: Int) {
+        themaCardImageService().getThemaCardImage(id: id) { result in
+            switch result {
+            case .success(let themaCardImageResponse):
+                self.themaCardImage = themaCardImageResponse
+                if let themaCardImage = self.themaCardImage {
+                    DispatchQueue.main.async {
+                        self.exploreView.themaCardCollectionView.reloadData()
+                        if self.themaCardInfo?.data.count ?? 0 > 0 {
+                            // self.exploreView.themaCardCollectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .centeredHorizontally, animated: false)
+                            // self.exploreView.themaCardCollectionView.setContentOffset(.zero, animated: false)
+                        }
+                    }
+                } else {
+                    print("RESPONSE ERROR")
+                }
+            case .failure(let error):
+                print("API Error: \(error)")
+            }
+        }
     }
 }
