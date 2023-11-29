c904c015ffe76e51ac83c9b1b2a9135a1e75c4ca
Zoe <Zoe.urssu@gmail.com>
2023-11-29 22:45:04 +0900

    [Chore] 네트워크 세팅

--- Airbnb-iOS/Source/Services/Config.swift
+++ Airbnb-iOS/Source/Services/Config.swift
@@ -20,4 +20,11 @@ enum Config {
         }
         return dict
     }()
+    
+    static let baseURL: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("Base URL is not set in plist for this configuration.")
+        }
+        return key
+    }()
 }
